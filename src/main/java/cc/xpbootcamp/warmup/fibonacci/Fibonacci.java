0be279f25eff02b1c887275058c29d0076688c2d
MengyaoLin <mengyao.lin@thoughtworks.com>
2020-02-15 20:24:26 +0800

    [Fibonacci]refactor implementation

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -4,12 +4,12 @@ public class Fibonacci {
 
   public long calculate(long position) {
 
-    if (position <= 0) {
-      throw new RuntimeException();
+    if (position == 1 || position == 2) {
+      return 1;
     } else if (position > 2) {
       return calculate(position - 1) + calculate(position - 2);
     } else {
-      return 1;
+      throw new RuntimeException();
     }
   }
 
